1234c6c40cdd01fe0ddba2d40e0f8a0f72fe8b54
igor <igor.savastsiuk@it-academy/by>
2023-12-03 18:50:42 +0200

    Remove filter :price_filter from app/helpers/taxon_filters_helper.rb

--- app/helpers/taxon_filters_helper.rb
+++ app/helpers/taxon_filters_helper.rb
@@ -4,7 +4,10 @@ require 'spree/core/product_filters'
 
 module TaxonFiltersHelper
   def applicable_filters_for(_taxon)
-    [:brand_filter, :price_filter].map do |filter_name|
+
+    # https://trello.com/c/1B1RWflA/5-investigate-how-to-remove-price-range-filter-and-remove-it
+    # [:brand_filter, :price_filter].map do |filter_name|
+    [:brand_filter].map do |filter_name|
       Spree::Core::ProductFilters.send(filter_name) if Spree::Core::ProductFilters.respond_to?(filter_name)
     end.compact
   end
